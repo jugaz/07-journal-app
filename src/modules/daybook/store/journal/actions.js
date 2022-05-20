aba764c8ddcd93e0c89ce67d5a57ea9ae4d1562e
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2022-05-19 21:57:55 -0500

    Journal Test Unit

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -2,56 +2,60 @@ import journalApi from "@/api/journalApi"
 // export const myAction = async ({ commit }) => {
 
 // }
-
 export const loadEntries = async ({ commit }) => {
 
     const { data } = await journalApi.get('/entries.json')
-    if( !data ) {
-        commit('setEntries',[])
-        return 
+
+    if ( !data ){
+        commit('setEntries', [] )
+        return
     }
 
     const entries = []
-    for (let id of Object.keys(data)) {
+    for( let id of Object.keys( data ) ) {
         entries.push({
             id,
             ...data[id]
         })
-        
     }
 
-    commit('setEntries',entries)
-
+    commit('setEntries', entries )
 }
 
-export const updateEntry = async ({ commit }, entry ) => {
-    const { date, picture, text } = entry 
+export const updateEntry = async ({ commit }, entry) => {  // entry debe de ser un parámetro
+
+    const { date, picture, text } = entry
     const dataToSave = { date, picture, text }
 
-    const resp = await journalApi.put(`entries/${entry.id}.json`, dataToSave)
-    console.log('UpdateEntry - Actions', resp)
-    
-    commit('updateEntry', {...entry} )
+    const resp = await journalApi.put( `/entries/${ entry.id }.json`, dataToSave )
+
+    dataToSave.id = entry.id
+
+    // Commit de una mutation -> updateEntry
+    commit('updateEntry', { ...dataToSave })
 }
 
-export const createEntry = async ( { commit }, entry ) => {
-    const { date, picture, text } = entry 
+
+export const createEntry = async ({ commit }, entry ) => {
+
+    // dataToSave
+    const { date, picture, text } = entry
     const dataToSave = { date, picture, text }
 
-    const { data } = await journalApi.post(`entries.json`, dataToSave)
+    const { data } = await journalApi.post( `entries.json`, dataToSave )
 
     dataToSave.id = data.name
 
-    commit('addEntry', {...entry} )
+    commit('addEntry', dataToSave )
 
     return data.name
 }
 
-export const deleteEntry = async ({ commit }, id) => {
 
-     await journalApi.delete(`entries/${id}.json`)
+export const deleteEntry = async ({ commit }, id ) => {
 
-     commit('deleteEntry', id )
+    await journalApi.delete(`/entries/${ id }.json`)
+    commit('deleteEntry', id)
 
-     return id
+    return id
 }
